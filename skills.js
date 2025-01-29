593661e670ae4297ccad7896255476070ed94072
nikhil shinde <nshinde@katalysttech.com>
2025-01-30 00:06:40 +0530

    Update : code

--- skills.js
+++ skills.js
@@ -2,3 +2,11 @@ function calculateNumbers(var1, var2)
 {
     return var1 + var2;
 }
+
+function getUserInput()
+{
+    var input1 = document.getElementById("input1").value;
+    var input2 = document.getElementById("input2").value;
+
+    return calculateNumbers(input1, input2);
+}
